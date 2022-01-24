e6f927577b76e4c61891ec1b094d6c24001bad91
Guillermo Tinoco <m3mo89@gmail.com>
2022-01-24 13:25:10 -0600

    Add FFImageLoading package

--- Movies.Android/MainActivity.cs
+++ Movies.Android/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using FFImageLoading.Forms.Platform;
 
 namespace Movies.Droid
 {
@@ -16,6 +17,7 @@ namespace Movies.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            CachedImageRenderer.Init(true);
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
