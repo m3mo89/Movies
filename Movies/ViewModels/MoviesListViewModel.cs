38c767d4278a88aa0b5670f1d08dbe1b5b0cc8db
Guillermo Tinoco <m3mo89@gmail.com>
2022-01-24 13:09:27 -0600

    Add MoviesListViewModel

---
+++ Movies/ViewModels/MoviesListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Movies.ViewModels.Base;
+
+namespace Movies.ViewModels
+{
+    public class MoviesListViewModel : BaseViewModel
+    {
+        public MoviesListViewModel()
+        {
+        }
+    }
+}
