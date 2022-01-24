479cc8e19b4ebf3c500fa8c5d304f7871d0886ab
Guillermo Tinoco <m3mo89@gmail.com>
2022-01-24 16:45:43 -0600

    Set selected language to empty

--- Movies/AppSettings.cs
+++ Movies/AppSettings.cs
@@ -6,7 +6,7 @@ namespace Movies
         public static string ApiKey = "0d05e9ad7331fbcd4d016dfc0c3bbe7c";
         public static string ApiUrl = "https://api.themoviedb.org/3/";
         public static string PosterImageUrl = "http://image.tmdb.org/t/p/w185/";
-        public static string SelectedLanguage = "en";
+        public static string SelectedLanguage = "";
         public static string USLanguage = "en-US";
         public static string MXLanguage = "es-MX";
         public static int Pages = 1;
