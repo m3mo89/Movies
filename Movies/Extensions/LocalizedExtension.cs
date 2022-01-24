07599f5f8fcf7ff0ed6d8340b8f4c4887cd26067
Guillermo Tinoco <m3mo89@gmail.com>
2022-01-24 12:52:04 -0600

    Add Localization project

---
+++ Movies/Extensions/LocalizedExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Localization;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Movies.Extensions
+{
+    [ContentProperty(nameof(ResourceName))]
+    public class LocalizedExtension : IMarkupExtension
+    {
+        public string ResourceName { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var currentCulture = AppResources.Culture;
+
+            return AppResources.ResourceManager.GetString(ResourceName, currentCulture);
+        }
+    }
+}
