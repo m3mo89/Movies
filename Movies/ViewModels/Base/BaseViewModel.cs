55aee1dc5d61699a32cbfabe9328848a5334f7f5
Guillermo Tinoco <m3mo89@gmail.com>
2022-01-24 13:08:16 -0600

    Add BaseViewModel

---
+++ Movies/ViewModels/Base/BaseViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Movies.ViewModels.Base
+{
+    public class BaseViewModel : BindableObject
+    {
+        private bool _isBusy;
+
+        public bool IsBusy
+        {
+            get
+            {
+                return _isBusy;
+            }
+
+            set
+            {
+                _isBusy = value;
+                OnPropertyChanged(nameof(IsBusy));
+            }
+        }
+
+        public virtual Task InitializeAsync(object navigationData)
+        {
+            return Task.FromResult(false);
+        }
+    }
+}
