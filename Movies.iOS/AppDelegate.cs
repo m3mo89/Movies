08d393444b7d03df7c87635f0c36d74800f3c120
Guillermo Tinoco <m3mo89@gmail.com>
2022-01-24 20:43:19 -0600

    Remove unnecessary namespaces

--- Movies.iOS/AppDelegate.cs
+++ Movies.iOS/AppDelegate.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using FFImageLoading.Forms.Platform;
 using Foundation;
-using Movies.iOS.Services;
-using Movies.Services.Database;
 using UIKit;
 
 namespace Movies.iOS
